3c4513a25c831b4ef1a2bfd2db33ee7e0a663322
Bernard Wiafe Akenteng <wiafe1713@gmail.com>
2026-07-30 10:15:34 -0700

    Update Landing_page.py

--- Landing_page.py
+++ Landing_page.py
@@ -168,7 +168,7 @@ col1, col2 = st.columns([1,1])
 with col1:
     st.link_button("Try Free Version", "https://uscs-classification-tool.streamlit.app")
 with col2:
-    st.link_button("View Source Code", "uscs-classification-tool.streamlit.app")
+    st.link_button("View Source Code", "https://github.com/IngBeno28/USCS-Classification-Tool")
 
 st.markdown("---")
 
